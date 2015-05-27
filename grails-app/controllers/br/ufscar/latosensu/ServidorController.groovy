5ea0d870660d01c50b29b601212d733c2d048a3a
Bruno <bruno.eng.comp@gmail.com>
2015-05-27 00:14:36 -0300

    Criacao de controller para Servidor que extende RestfulController

---
+++ grails-app/controllers/br/ufscar/latosensu/ServidorController.groovy
@@ -0,0 +1,10 @@
+package br.ufscar.latosensu
+
+import grails.rest.RestfulController
+
+class ServidorController extends RestfulController {
+    static responseFormats = ['json', 'xml']
+    ServidorController() {
+        super(Servidor)
+    }
+}
